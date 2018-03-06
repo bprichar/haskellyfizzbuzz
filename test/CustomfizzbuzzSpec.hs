6dc13093579bb01dc45c44b8dae8ef3e838f1887
Brad Richardson <brichardson@structint.com>
2018-03-05 20:39:52 -0800

    Add next property check and limit test size

--- test/CustomfizzbuzzSpec.hs
+++ test/CustomfizzbuzzSpec.hs
@@ -1,9 +1,10 @@
 module CustomfizzbuzzSpec (main, spec) where
 
-import           Customfizzbuzz  (FizzbuzzSpec, FizzbuzzSpecPart,
-                                  customfizzbuzz)
+import           Customfizzbuzz             (FizzbuzzSpec, FizzbuzzSpecPart,
+                                             customfizzbuzz)
 import           Data.List
 import           Test.Hspec
+import           Test.Hspec.Core.QuickCheck (modifyMaxSuccess)
 import           Test.QuickCheck
 import           Text.Read
 
@@ -11,16 +12,19 @@ main = hspec spec
 
 spec :: Spec
 spec =
-    describe "fizzbuzz" $ do
+    describe "fizzbuzz"$ modifyMaxSuccess (const 10) $ do
         it "Each item is a number, or some combination of the words" $
             forAll specGenerator propPossibleWordsOrNumbers
 
         it "If item is a number, it is the position in the list" $
             forAll specGenerator propIfNumberIsPosition
 
+        it "Each word is included every specified position" $
+            forAll specGenerator propEachWordAtEveryNthPosition
+
 specGenerator :: Gen FizzbuzzSpec
 specGenerator = do
-    n <- choose (1, 5)
+    n <- choose (1, 4)
     vectorOf n specPartGenerator
 
 specPartGenerator :: Gen FizzbuzzSpecPart
@@ -35,7 +39,7 @@ availableWords :: [String]
 availableWords = ["Fizz", "Buzz", "Hiss", "Howl", "Foo", "Bar", "Baz", "Quux"]
 
 somePrimes :: [Int]
-somePrimes = [2, 3, 5, 7, 11, 13, 17, 23]
+somePrimes = [2, 3, 5, 7, 11, 13, 17]
 
 propPossibleWordsOrNumbers :: FizzbuzzSpec -> Bool
 propPossibleWordsOrNumbers fizzbuzzspec =
@@ -76,3 +80,18 @@ propIfNumberIsPosition fizzbuzzspec =
 
 ifNumberIsPosition :: (String, Int) -> Bool
 ifNumberIsPosition (str, pos) = not (isNumber str) || (show pos == str)
+
+propEachWordAtEveryNthPosition :: FizzbuzzSpec -> Bool
+propEachWordAtEveryNthPosition fizzbuzzspec =
+    all
+        (wordAtEveryNthPosition (specToFizzbuzzes fizzbuzzspec))
+        fizzbuzzspec
+
+wordAtEveryNthPosition :: [String] -> FizzbuzzSpecPart -> Bool
+wordAtEveryNthPosition fizzbuzzes (wrd, freq) =
+    all (wrd `isInfixOf`) (every freq fizzbuzzes)
+
+every :: Int -> [a] -> [a]
+every n xs = case drop (n-1) xs of
+    (y:ys) -> y : every n ys
+    []     -> []
