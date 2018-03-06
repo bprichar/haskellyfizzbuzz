42b1cd37e92e1f536097f134d00f6734be2e4341
Brad Richardson <brichardson@structint.com>
2018-03-05 20:43:57 -0800

    Finalize properties
     - Have to have unique multiple for each word

--- test/CustomfizzbuzzSpec.hs
+++ test/CustomfizzbuzzSpec.hs
@@ -22,18 +22,19 @@ spec =
         it "Each word is included every specified position" $
             forAll specGenerator propEachWordAtEveryNthPosition
 
+        it "If item is number, its position is not divisible by any number in spec" $
+            forAll specGenerator propNumbersNotDivisibleBySpecNumbers
+
+        it "If item is a word, its position is divisible by the product of the individual word spec numbers" $
+            forAll specGenerator propWordsDivisibleBySpecProduct
+
 specGenerator :: Gen FizzbuzzSpec
 specGenerator = do
     n <- choose (1, 4)
-    vectorOf n specPartGenerator
-
-specPartGenerator :: Gen FizzbuzzSpecPart
-specPartGenerator = do
-    i <- choose (0, length availableWords - 1)
-    j <- choose (0, length somePrimes - 1)
-    let thisWord = availableWords !! i
-    let thisPrime = somePrimes !! j
-    return (thisWord, thisPrime)
+    words' <- shuffle availableWords
+    primes <- shuffle somePrimes
+    let specs = zip words' primes
+    return $ take n specs
 
 availableWords :: [String]
 availableWords = ["Fizz", "Buzz", "Hiss", "Howl", "Foo", "Bar", "Baz", "Quux"]
@@ -95,3 +96,44 @@ every :: Int -> [a] -> [a]
 every n xs = case drop (n-1) xs of
     (y:ys) -> y : every n ys
     []     -> []
+
+propNumbersNotDivisibleBySpecNumbers :: FizzbuzzSpec -> Bool
+propNumbersNotDivisibleBySpecNumbers fizzbuzzspec =
+    all
+        (ifNumberPositionNotDivisibleByAnyOf (map snd fizzbuzzspec))
+        (zip (specToFizzbuzzes fizzbuzzspec) [1..])
+
+ifNumberPositionNotDivisibleByAnyOf :: [Int] -> (String, Int) -> Bool
+ifNumberPositionNotDivisibleByAnyOf divisors (str, pos) =
+    not (isNumber str) || all (pos `notDivisibleBy`) divisors
+
+notDivisibleBy :: Int -> Int -> Bool
+notDivisibleBy num divisor = num `mod` divisor /= 0
+
+propWordsDivisibleBySpecProduct :: FizzbuzzSpec -> Bool
+propWordsDivisibleBySpecProduct fizzbuzzspec =
+    all
+        (ifNotNumberPositionDivisbleByCombinedWordSpecProduct specCombinations)
+        (zip (specToFizzbuzzes fizzbuzzspec) [1..])
+    where specCombinations = allSpecCombinations fizzbuzzspec
+
+ifNotNumberPositionDivisbleByCombinedWordSpecProduct :: [(String, Int)] -> (String, Int) -> Bool
+ifNotNumberPositionDivisbleByCombinedWordSpecProduct specCombinations (str, pos) =
+    isNumber str || case whichSpecWord of
+        Just index -> pos `divisbleBy` (specProducts !! index)
+        Nothing    -> False
+    where
+        whichSpecWord = elemIndex str specWords
+        specWords = map fst specCombinations
+        specProducts = map snd specCombinations
+
+divisbleBy :: Int -> Int -> Bool
+divisbleBy num divisor = num `mod` divisor == 0
+
+allSpecCombinations :: FizzbuzzSpec -> [(String, Int)]
+allSpecCombinations fizzbuzzspec = map foldSpecs allCombinations
+    where
+        foldSpecs = foldr combineSpecs ("", 1)
+        combineSpecs (wrd1, mul1) (wrd2, mul2) = (wrd1 ++ wrd2, mul1 * mul2)
+        allCombinations = concatMap (`combinations` fizzbuzzspec) [1..len']
+        len' = length fizzbuzzspec
